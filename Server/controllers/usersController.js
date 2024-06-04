98fc838611a3f25fce3151874dbbb21cef50de73
naamashvalb <naamah.shv@gmail.com>
2024-06-04 17:59:05 +0300

    change

---
+++ Server/controllers/usersController.js
@@ -0,0 +1,126 @@
+import { UsersService } from '../services/usersService.js'
+const usersService = new UsersService();
+export class UsersController {
+
+    async getUsers(req, res, next) {
+        try {
+            // const limit = res.query['limit']
+            console.log("user service: ", req.query);
+            const resultItems = await usersService.getUsers(req.query)
+                return res.status(200).json(resultItems);
+        }
+        catch (ex) {
+            const err = {}
+            err.statusCode = 500;
+            err.message = ex;
+            next(err)
+        }
+    }
+
+    async getUserById(req, res, next) {
+        try {
+            
+            const resultItem = await usersService.getUserById(req.params.id);
+                res.status(200).json({ status: 200, data: resultItem });
+        }
+        catch (ex) {
+            const err = {}
+            err.statusCode = 500;
+            err.message = ex;
+            next(err)
+        }
+    }
+
+
+    async addUser(req, res, next) {
+        try {
+            const result = await usersService.addUser(req.body);
+                res.status(201).json({insertId: result.insertId});
+        }
+        catch (ex) {
+            const err = {}
+            err.statusCode = 500;
+            err.message = ex;
+            next(err)
+        }
+    }
+
+
+    async deleteUser(req, res, next) {
+        try {
+            console.log("users");
+            console.log(req.params.id);
+            const userDetails = await usersService.getUserById(req.params.id);
+            authorizeUser(userDetails[0].id, req.user.id, res);
+            const response = await usersService.deleteUser(req.params.id);
+                res.status(response.affectedRows?204:404).send();
+        }
+        catch (ex) {
+            const err = {}
+            err.statusCode = 500;
+            err.message = ex;
+            next(err)
+        }
+    }
+
+    async updateUser(req, res, next) {
+        try {
+            console.log("users");
+            console.log(req.params.id);
+            console.log(req.body);
+            const response = await usersService.updateUser(req.body, req.params.id);
+                res.status(response.affectedRows?204:404).send();
+        }
+        catch (ex) {
+            const err = {}
+            err.statusCode = 500;
+            err.message = ex;
+            next(err)
+        }
+    }
+
+    async getUsersAlbums(req, res, next){
+        try {
+
+            const resultItems = await usersService.getUsersAlbums(req.params.id)
+                return res.status(200).json(resultItems);
+        }
+        catch (ex) {
+            const err = {}
+            err.statusCode = 500;
+            err.message = ex;
+            next(err)
+        }
+    }
+
+    async getUsersPosts(req, res, next){
+        try {
+
+            const resultItems = await usersService.getUsersPosts(req.params.id)
+                return res.status(200).json(resultItems);
+        }
+        catch (ex) {
+            const err = {}
+            err.statusCode = 500;
+            err.message = ex;
+            next(err)
+        }
+    }
+
+    async getUsersTodos(req, res, next){
+        try {
+
+            const resultItems = await usersService.getUsersTodos(req.params.id)
+                return res.status(200).json(resultItems);
+        }
+        catch (ex) {
+            const err = {}
+            err.statusCode = 500;
+            err.message = ex;
+            next(err)
+        }
+    }
+
+
+}
+
---
+++ Server/routes/usersRoute.js
@@ -0,0 +1,32 @@
+import express from "express";
+import { UsersController } from '../controllers/usersController.js';
+import { UsersService } from "../services/usersService.js";
+import { validateUserData,validate  } from "../middleware/validationMiddleware.js";
+import { validationResult } from "express-validator";
+//import { authorizeUser } from "../middleware/authorizationMiddleware.js";
+const usersRouter = express.Router();
+
+const userscontroller = new UsersController();
+
+usersRouter.get("/:id", userscontroller.getUserById);
+usersRouter.get("/", userscontroller.getUsers);
+
+//usersRouter.get("/:id/posts", userscontroller.getUsersPosts);
+//usersRouter.get("/:id/albums", userscontroller.getUsersAlbums);
+usersRouter.post("/",validateUserData,(req, res, next)=>{
+    const errors = validationResult(req);
+    if (!errors.isEmpty())
+        return res.status(400).json({ errors: errors.array() });
+    next();}, userscontroller.addUser);
+
+usersRouter.delete("/:id", userscontroller.deleteUser);
+//usersRouter.use((req,res,next)=>{authorizeUser(req.body.id,req.user.id,res); next();});
+usersRouter.put("/:id",validateUserData,(req, res, next)=>{
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+    next();}, userscontroller.updateUser);
+usersRouter.get("/:id/todos", userscontroller.getUsersTodos);
+
+export default usersRouter
